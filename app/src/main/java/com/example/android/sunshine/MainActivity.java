d5444dd02133c18c675c1b8f3803474564dc957d
Ashwin Nikam <anikam@buffalo.edu>
2017-06-30 22:19:54 -0400

    Initial fetch weather data

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.android.sunshine;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,32 +20,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_forecast);
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        String[] dummyWeatherData = {
-                                "Today, May 17 - Clear - 17°C / 15°C",
-                                "Tomorrow - Cloudy - 19°C / 15°C",
-                                "Thursday - Rainy- 30°C / 11°C",
-                                "Friday - Thunderstorms - 21°C / 9°C",
-                                "Saturday - Thunderstorms - 16°C / 7°C",
-                                "Sunday - Rainy - 16°C / 8°C",
-                                "Monday - Partly Cloudy - 15°C / 10°C",
-                                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                                "Thu, May 26 - Stormy - 30°C / 11°C",
-                                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                                "Sat, May 28 - Meteors - 16°C / 7°C",
-                                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-                        };
+        loadWeatherData();
+    }
 
-        for(String dummyWeatherDay: dummyWeatherData){
-            mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
+    public class FetchWeatherTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String mWeatherData = null;
+            URL url = urls[0];
+            try {
+                mWeatherData = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return mWeatherData;
         }
 
-        
-
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mWeatherTextView.append(s);
+        }
     }
+
+    private void loadWeatherData(){
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        URL url = NetworkUtils.buildUrl(location);
+        new FetchWeatherTask().execute(url);
+    }
+
+
 }
