3ccaefb191e7e02f6739b25d56011b46fec8e55a
Ashwin Nikam <anikam@buffalo.edu>
2017-07-02 17:33:31 -0400

    Added preferences

---
+++ app/src/main/java/com/example/android/sunshine/SettingsFragment.java
@@ -0,0 +1,67 @@
+package com.example.android.sunshine;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+/**
+ * Created by ashwin on 7/2/17.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener{
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_forecast);
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+
+        for(int i=0; i<count; i++){
+            Preference preference = preferenceScreen.getPreference(i);
+            if(!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        Preference preference = findPreference(s);
+        if(preference != null){
+            String value = sharedPreferences.getString(s, "");
+            setPreferenceSummary(preference, value);
+        }
+    }
+
+    private void setPreferenceSummary(Preference preference, String value){
+        if(preference instanceof ListPreference){
+            int index = ((ListPreference) preference).findIndexOfValue(value);
+            preference.setSummary(((ListPreference) preference).getEntries()[index]);
+        } else {
+            preference.setSummary(value);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+}
